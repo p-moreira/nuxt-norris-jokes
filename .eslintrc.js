7a56da2b68fe303b73cd4c656f68a9958a96f74f
pmoreira <pmoreira@firenzze.com>
2020-04-04 12:27:37 -0300

    Initial version

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        '@nuxtjs',
+        'plugin:nuxt/recommended'
+    ],
+    // add your custom rules here
+    rules: {
+        "vue/multiline-html-element-content-newline": [
+            "error",
+            {
+                ignoreWhenEmpty: true,
+                allowEmptyLines: true
+            }
+        ],
+        "vue/html-indent": ["error", 4],
+        "indent": ["error", 4],
+        "semi": ["error", "never", { "beforeStatementContinuationChars": "never" }]
+    }
+}
